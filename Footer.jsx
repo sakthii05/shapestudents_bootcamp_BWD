585fd8d927204ab518b220d740a915c9ded688fe
sakthii05 <86524610+sakthii05@users.noreply.github.com>
2021-06-27 13:32:08 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+    return (
+      <div>
+        <footer>
+          <p>copyrights by shapestudents @ {new Date().getFullYear()}</p>
+          </footer>
+          </div>
+             );
+}
+export default Footer;
